9ef239c55b1ba5ae91156394c62de674a24f7b9d
Spikree <avimahalingpure10@gmail.com>
2026-09-18 00:49:43 +0100

    add user routes to access the user routes, used express.json to accept requests and cookie parser to parse cookies

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import express from "express";
 import cors from 'cors';
 import dotenv from "dotenv";
 import connectDb from "./libs/connectToDb.js";
+import cookieParser from "cookie-parser"
+
+import UserRouter from "./routes/user.route.js"
 
 dotenv.config();
 const app = express();
 app.use(cors());
 const port = 2000;
+app.use(express.json());
+app.use(cookieParser());
 
 connectDb();
 
 app.get('/', (req, res) => {
   res.send("backend working");
 });
+
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
   console.log(`backend running on port ${port}`);
