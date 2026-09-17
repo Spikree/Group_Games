e30ba624988e3b2429272ee056c30f1f38a3e3d7
Spikree <avimahalingpure10@gmail.com>
2026-09-18 00:44:36 +0100

    connect three routes with their controller: tested

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { register } from "../controller/user.controller";
-import upload from "../middleware/multer.middleware";
+import { login, logout, register } from "../controller/user.controller.js";
+import upload from "../middleware/multer.middleware.js";
+import verifyToken from "../middleware/verifyToken.middleware.js";
 
 const router = Router();
 
-router.post("/register",upload.single("profilePicture"),register);
+router.post("/register", upload.single("profilePicture"), register);
+router.post("/login", login);
+router.post("/logout",verifyToken, logout);
+
+export default router;
